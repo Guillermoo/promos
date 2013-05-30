467869d897c24e005c86d4c6f576e4a4a8f61c4e
Hugo Langa <hugoepila@gmail.com>
2013-05-30 02:02:57 +0200

    agregados elementos de menú a los lados a modo de ejemplo. Nueva hoja de estilos

--- themes/classic/views/layouts/column3.php
+++ themes/classic/views/layouts/column3.php
@@ -1,13 +1,20 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/main'); ?>
 <div class="span-5">
-	<div>
+	<div class="categoriasizda">
 		<h4>Categorías:</h4>
 		<?php 
-		$this->beginWidget('zii.widgets.CPortlet', array(
-			'title'=>'Operations',
-		));
-		$this->endWidget();
+		$this->widget('zii.widgets.CMenu', array(
+    'items'=>array(
+        // Important: you need to specify url as 'controller/action',
+        // not just as 'controller' even if default acion is used.
+        array('label'=>'Salud y belleza', 'url'=>array('site/index')),
+        // 'Products' menu item will be selected no matter which tag parameter value is since it's not specified.
+        array('label'=>'Calzado', 'url'=>array('product/index')),        
+        array('label'=>'Bodas', 'url'=>array('site/login')),
+        array('label'=>'Visutería', 'url'=>array('site/login')),
+    ),
+));	
 		?>
 	</div>
 </div>
@@ -19,9 +26,24 @@
 </div>
 
 <div class="span-5">
-	<div class="logindcha">
+	<?php if(Yii::app()->user->isGuest){ ?>
+	<div class="logindcha">		
 		<h4>Login:</h4>
+		<?php 
+		$this->widget('zii.widgets.CMenu', array(
+    'items'=>array(        
+        array('label'=>'Logueo', 'url'=>array('user/login'), 'visible'=>Yii::app()->user->isGuest),            
+    ),
+));	
+		?>
 	</div>
+	<?php }else{
+		?>
+		<div class="logindcha">
+			<?php echo "<h4>Hola ".Yii::app()->user->getName()."</h4>"; ?>
+		</div>
+		<?php
+	} ?>
 	<div class="tagsdcha">
 		<h4>Tags:</h4>
 	</div>
--- themes/classic/views/layouts/main.php
+++ themes/classic/views/layouts/main.php
@@ -14,6 +14,7 @@
 
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/cierzodevs.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 	
