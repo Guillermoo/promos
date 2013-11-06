9b22f395d3fc3246cd6aa7f73d3354733dda1c90
Guillermoo <grillermo@gmail.com>
2013-11-06 08:13:29 +0100

    Cabmios home

--- themes/frontEnd/views/site/index.php
+++ themes/frontEnd/views/site/index.php
@@ -176,115 +176,12 @@
 						</div>
 					</li>
 				<?php endforeach; ?>
-				<!--<li class="span3">
-					<div class="thumbnail light">
-						<a href="#">
-							<span class="label label-info price">&euro; 2,<sup>99</sup></span>
-							<span class="label label-important price price-over">&euro; 1,<sup>99</sup></span>
-							<img data-hover="<?php echo Yii::app()->request->baseUrl.'/themes/frontEnd'; ?>/img/product_04b.jpg" src="img/product_04.jpg" alt="">
-						</a>
-						<div class="caption">
-							<a href="#">Product A</a>
-						</div>
-						<a href="#" class="btn btn-block">all products in category</a>
-					</div>
-				</li>
-				<li class="span3">
-					<div class="thumbnail dark">
-						<a href="#">
-							<div class="label label-info price">&euro; 93,<sup>99</sup></div>
-							<img data-hover="<?php echo Yii::app()->request->baseUrl.'/themes/frontEnd'; ?>/img/product_05b.jpg" src="img/product_05.jpg" alt="">
-						</a>
-						<div class="caption">
-							<a href="#">Product B</a>
-						</div>
-						<a href="#" class="btn btn-block">all products in category</a>
-					</div>
-				</li>
-				<li class="span3">
-					<div class="thumbnail light">
-						<a href="#">
-							<div class="label label-info price">&euro; 1023,<sup>99</sup></div>
-							<img data-hover="<?php echo Yii::app()->request->baseUrl.'/themes/frontEnd'; ?>/img/product_06b.jpg" src="img/product_06.jpg" alt="">
-						</a>
-						<div class="caption">
-							<a href="#">Product C</a>
-						</div>
-						<a href="#" class="btn btn-block">all products in category</a>
-					</div>
-				</li>
-				<li class="span3">
-					<div class="thumbnail dark">
-						<a href="#">
-							<div class="label label-info price">&euro; 123,<sup>99</sup></div>
-							<span class="label label-important price price-over">&euro; 122,<sup>99</sup></span>
-							<img data-hover="<?php echo Yii::app()->request->baseUrl.'/themes/frontEnd'; ?>/img/product_07b.jpg" src="img/product_07.jpg" alt="">
-						</a>
-						<div class="caption">
-							<a href="#">Product D</a>
-						</div>
-						<a href="#" class="btn btn-block">Todos los productos de la categoría</a>
-					</div>
-				</li>-->
+				
 			</ul>
 		</div>
 	</div>
 
-	<!--<div class="row-fluid">
-		<div class="span12">
-			<ul class="thumbnails product-list-inline-large">
-				<li class="span3">
-					<div class="thumbnail dark">
-						<a href="#">
-							<span class="label label-info price">&euro; 2,<sup>99</sup></span>
-							<img data-hover="<?php echo Yii::app()->request->baseUrl.'/themes/frontEnd'; ?>/img/product_08b.jpg" src="img/product_08.jpg" alt="">
-						</a>
-						<div class="caption">
-							<a href="#">Product A</a>
-						</div>
-						<a href="#" class="btn btn-block">all products in category</a>
-					</div>
-				</li>
-				<li class="span3">
-					<div class="thumbnail light">
-						<a href="#">
-							<div class="label label-info price">&euro; 93,<sup>99</sup></div>
-							<img data-hover="<?php echo Yii::app()->request->baseUrl.'/themes/frontEnd'; ?>/img/product_09b.jpg" src="img/product_09.jpg" alt="">
-						</a>
-						<div class="caption">
-							<a href="#">Product B</a>
-						</div>
-						<a href="#" class="btn btn-block">all products in category</a>
-					</div>
-				</li>
-				<li class="span3">
-					<div class="thumbnail dark">
-						<a href="#">
-							<div class="label label-info price">&euro; 1023,<sup>99</sup></div>
-							<span class="label label-important price price-over">&euro; 999,<sup>99</sup></span>
-							<img data-hover="<?php echo Yii::app()->request->baseUrl.'/themes/frontEnd'; ?>/img/product_10b.jpg" src="img/product_10.jpg" alt="">
-						</a>
-						<div class="caption">
-							<a href="#">Product C</a>
-						</div>
-						<a href="#" class="btn btn-block">all products in category</a>
-					</div>
-				</li>
-				<li class="span3">
-					<div class="thumbnail light">
-						<a href="#">
-							<div class="label label-info price">&euro; 123,<sup>99</sup></div>
-							<img data-hover="<?php echo Yii::app()->request->baseUrl.'/themes/frontEnd'; ?>/img/product_11b.jpg" src="img/product_11.jpg" alt="">
-						</a>
-						<div class="caption">
-							<a href="#">Product D</a>
-						</div>
-						<a href="#" class="btn btn-block">all products in category</a>
-					</div>
-				</li>
-			</ul>
-		</div>
-	</div>-->
+	
 
 	
 
