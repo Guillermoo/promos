5cc6a4514765fcd3f71128ebbc83eadcfd4327b9
Hugo Langa <hugoepila@gmail.com>
2013-11-06 02:01:23 +0100

    Mejorada estructura de vistas en Frontend. Implementado listado de empresas y detalle de cada empresa

--- themes/frontEnd/views/layouts/main.php
+++ themes/frontEnd/views/layouts/main.php
@@ -34,7 +34,34 @@
 
 	</head>
 
-	<body>
+	<body>		
+		<div class="navbar pull-right header-nav" id="superior">
+			<ul class="nav">
+				<li>
+					<?php echo CHtml::link('Login','user/login');?>							
+					<ul class="dropdown-menu">
+						<li>
+							<div class="dropdown-content">
+								<br>
+								<form>
+									<input type="text" class="input-medium" placeholder="Username"><br>
+									<input type="password" class="input-medium" placeholder="Password"><br>
+									<button class="btn">reset</button>
+									<button class="btn btn-primary">login</button>
+								</form> 
+								<br>
+							</div>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<?php echo CHtml::link('Nueva Empresa','user/registrationcompany');?>	
+				</li>
+				<li>
+					<?php echo CHtml::link('Nuevo Comprador','user/registrarcomprador');?>	
+				</li>
+			</ul>
+		</div>
 	<?php if(!empty(Yii::app()->params['debugContent'])):?>
 	                <?php echo Yii::app()->params['debugContent'];?>
 	  <?php endif;?>
@@ -46,6 +73,54 @@
 
 		<!-- Use class "container-fluid" on the following div for making complete website fluid -->
 		<div class="container">
+
+			<div class="row-fluid print-hide">
+				<div class="span4">
+					<div class="header-action">
+
+					</div>
+				</div>
+			</div>
+
+			<div class="row-fluid print-hide">
+				<div class="span3 logo">
+					<img src="<?php echo Yii::app()->request->baseUrl.'/themes/frontEnd'; ?>/img/logo.png" alt="Logo">
+				</div>
+				<div class="span4">
+					<form class="form-search header-search">
+						<div class="input-append">
+							<input class="input-large search-query" type="text"placeholder="Buscar productos...">
+							<button class="btn" type="submit">buscar</button>
+						</div>
+					</form>
+				</div>
+					
+				<br>
+				<div class="span5">
+					<img src="<?php echo Yii::app()->request->baseUrl.'/themes/frontEnd'; ?>/img/banner_top.jpg" alt="No shipping">
+				</div>
+			</div>
+
+			<div class="row-fluid print-hide">
+				<div class="span12">
+					<div class="navbar main-nav">
+						<div class="navbar-inner">
+							<div class="container">
+								<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">menu</a>
+								<div class="nav-collapse">
+									<ul class="nav">
+										<li class="active"><a href="index.html"><i class="icon-home"></i></a></li>
+										<li class="divider-vertical"></li>
+										<li><?php echo Chtml::link('Categorias',array('categoria/index'))?></li>
+										<li><?php echo Chtml::link('Empresas',array('Empresa/index'))?></li>										
+									</ul>										
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>			
+
 			<?php echo $content; ?>
 			
 			
