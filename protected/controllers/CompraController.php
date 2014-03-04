1adcca33d6dfdbfa43d1d5c822d56a33edf1889c
Hugo Langa <hugoepila@gmail.com>
2014-03-04 18:53:42 +0100

    Se verifican y se guardan los datos de la compra de un bono. Se actualiza la cuenta del usuario. Con SandBox.

--- protected/controllers/CompraController.php
+++ protected/controllers/CompraController.php
@@ -393,7 +393,7 @@ class CompraController extends Controller
 			$idBono = 0;
 
 			$mail_From    = "proemocion@proemocion.com";
-	      	$mail_To      = "hugoepila@gmail.com";
+	      	$mail_To      = "proemocion@proemocion.com";
 	      	$mail_Subject = "Ejecutado IPN";
 
 			//cojo el id_usuario y el id_promo del campo custom
@@ -404,8 +404,8 @@ class CompraController extends Controller
 			}
 
 			//QUITAR LO SIGUIENTE PARA PRODUCCIÓN
-			$idUsuario = 11;
-			$idBono = 1;
+			//$idUsuario = 11;
+			//$idBono = 1;
 
 			if (!$fp) {
 				// HTTP ERROR
@@ -429,7 +429,7 @@ class CompraController extends Controller
                             $emailtext .= $key . " = " .$value ."\n\n";
                         }
  
-                        mail($mail_To, $mail_Subject, $emailtext . "\n\n" . $mail_Body, $mail_From);
+                       // mail($mail_To, $mail_Subject, $emailtext . "\n\n" . $mail_Body, $mail_From);
 
 						// check the payment_status is Completed				
 						//fputs($DescriptorFichero,'Estatus: '.$payment_status); 						
@@ -437,7 +437,10 @@ class CompraController extends Controller
 							
 						}else{
 							// Comprobar que el txn_id no se ha procesado todavía
-							$compra = UsersCuentas::model()->find('referencia='.$txn_id);				
+							$compra = UsersCuentas::model()->find('referencia='.$txn_id);
+							if($compra){
+								return;
+							}		
 							
 							// check that payment_amount/payment_currency are correct
 
@@ -449,17 +452,33 @@ class CompraController extends Controller
 							
 							$ids = explode('_',$custom);
 							$idUsuario = $ids[0];
-							$idPromocion = $ids[1];
+							$idBono = $ids[1];
 
-							if(!empty($idUsuario) && !empty($idPromocion)){
+							if(!empty($idUsuario) && !empty($idBono)){
 
-							//guardo los datos de la compra
-							$this->insertarCompraBono($idUsuario,$idBono,$txn_id,$precio,$custom,$fecha_fin);
+								$bono = Cuenta::model()->find('id=:id',array(':id'=>$idBono));	
 
-							//actualizo los datos del usuario-empresa
+								if($bono->precio != $precio){
+									 mail($mail_To, $mail_Subject,'El usuario con id: '.$idUsuario.' ha comprado el Bono con id: '.$idBono.' pero no coincide el precio que ha pagado con el que marca el Bono: Pagado -> '.$precio.', precio indicado: '.$bono->precio, $mail_From);
+								}
+
+								//guardo los datos de la compra
+								$this->insertarCompraBono($idUsuario,$idBono,$txn_id,$precio,$custom,$fecha_fin);
+
+								//actualizo los datos del usuario-empresa
+								$profile = Profile::model()->find('id=:id',array(':id'=>$idUsuario));
+
+								$profile->tipocuenta = $idBono;
+
+								if($profile->update()){							
+									//enviar email a proemocion para informar de la compra						
+									Yii::app()->getModule('user')->sendMail(Yii::app()->params['websiteEmail'],'Nuevo BONO',$message);	
+								}else{
+									 mail($mail_To, $mail_Subject,'El usuario con id: '.$idUsuario.' ha comprado el Bono con id: '.$idBono.' pero no se ha podido almacenar en la Base de Datos '.$bono->precio, $mail_From);
+								}
 							
 							}else{
-								Yii::app()->getModule('user')->sendMail($payer_email,'Compra promocion no verificada','No se ha podido recuperar todos los datos del comprador o del Bono.');	
+								Yii::app()->getModule('user')->sendMail($payer_email,'Compra Bono no verificada','No se ha podido recuperar todos los datos del comprador o del Bono.');	
 							}
 												
 						}					
@@ -481,92 +500,16 @@ class CompraController extends Controller
 
 					}
 				}
-				fclose ($fp);	
-
-				//enviar email a proemocion para informar de la compra						
-				//Yii::app()->getModule('user')->sendMail(Yii::app()->params['websiteEmail'],'Nuevo BONO',$message);									      		      	
+				fclose ($fp);									      		      	
 			}		
 		} 
 	}
 
-	public function actionCheckoutBonoNew(){
-		// Send an empty HTTP 200 OK response to acknowledge receipt of the notification 
-   		header('HTTP/1.1 200 OK');
-
-   		// Build the required acknowledgement message out of the notification just received
-		$req = 'cmd=_notify-validate';               // Add 'cmd=_notify-validate' to beginning of the acknowledgement
-
-		foreach ($_POST as $key => $value) {         // Loop through the notification NV pairs
-			$value = urlencode(stripslashes($value));  // Encode these values
-		    $req  .= "&$key=$value";                   // Add the NV pairs to the acknowledgement
-		}
-
-		// Set up the acknowledgement request headers
-	  $header  = "POST /cgi-bin/webscr HTTP/1.1\r\n";                    // HTTP POST request
-	  $header .= "Content-Type: application/x-www-form-urlencoded\r\n";
-	  $header .= "Content-Length: " . strlen($req) . "\r\n\r\n";
-
-	  // Open a socket for the acknowledgement request
-	  $fp = fsockopen('www.sandbox.paypal.com', 80, $errno, $errstr, 30);
-
-	  // Send the HTTP POST request back to PayPal for validation
-	  fputs($fp, $header . $req);
-
-
-	  //COMPROBAR QUE LA INFORMACIÓN RECIBIDA ESTÁ BIEN
-	  while (!feof($fp)) {                     // While not EOF
-	    $res = fgets($fp, 1024);               // Get the acknowledgement response
-	    if (strcmp ($res, "VERIFIED") == 0) {  // Response contains VERIFIED - process notification
-
-	      // Send an email announcing the IPN message is VERIFIED
-	      $mail_From    = "IPN@example.com";
-	      $mail_To      = "hugoepila@gmail.com";
-	      $mail_Subject = "VERIFIED IPN";
-	      $mail_Body    = $req;
-	      mail($mail_To, $mail_Subject, $mail_Body, $mail_From);
-
-	      // Authentication protocol is complete - OK to process notification contents
-
-	      // Possible processing steps for a payment include the following:
-
-	      // Check that the payment_status is Completed
-	      // Check that txn_id has not been previously processed
-	      // Check that receiver_email is your Primary PayPal email
-	      // Check that payment_amount/payment_currency are correct
-	      // Process payment
-
-	    } 
-	    else if (strcmp ($res, "INVALID") == 0) { 
-	      //Response contains INVALID - reject notification
-
-	      // Authentication protocol is complete - begin error handling
-
-	      // Send an email announcing the IPN message is INVALID
-	      $mail_From    = "IPN@example.com";
-	      $mail_To      = "hugoepila@gmail.com";
-	      $mail_Subject = "INVALID IPN";
-	      $mail_Body    = $req;
-
-	      mail($mail_To, $mail_Subject, $mail_Body, $mail_From);
-	    }else{
-	    	// Send an email announcing the IPN message is VERIFIED
-	      $mail_From    = "IPN@example.com";
-	      $mail_To      = "hugoepila@gmail.com";
-	      $mail_Subject = "Ejecutado IPN";
-	      $mail_Body    = "res = ".$res."  |  req = ".$req;
-	      mail($mail_To, $mail_Subject, $mail_Body, $mail_From);
-	    }
-	  }
-
-	   fclose($fp);  // Close the file
-	    
-   	}
-
-   	public function insertarCompraBono($idUsuario,$idPromocion,$referencia,$precio,$custom,$fecha_fin){			
+   	public function insertarCompraBono($idUsuario,$idBono,$referencia,$precio,$custom,$fecha_fin){			
 			$model=new UsersCuentas;
 
 			$model->id_usuario = $idUsuario;
-			$model->id_cuenta = $idPromocion;
+			$model->id_cuenta = $idBono;
 			$model->referencia = $referencia;
 			$model->cant_pagado = $precio;
 			$model->fecha_inicio = date("Y-m-d H:i:s");
