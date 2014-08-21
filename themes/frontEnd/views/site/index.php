fe805488dcc827ac80cd7acb776fff0c06540698
HugoL <hugoepila@gmail.com>
2014-08-21 22:51:58 +0200

    Cambios en la página principal de lanzamiento

--- themes/frontEnd/views/site/index.php
+++ themes/frontEnd/views/site/index.php
@@ -1,39 +1,66 @@
-<div class="row-fluid print-show">
-	<div class="span12">
-		Proemoción - Tu web de promociones
-	</div>
-</div>			
 
-<br/>	
 
-<div class="row-fluid">
-	<div class="span12">
-		<div class="slider-wrapper theme-bar">
-			<div class="ribbon"></div>
-			<div id="slider1" class="nivoslider">
-				<img src="<?php echo Yii::app()->request->baseUrl.'/themes//frontEnd'; ?>/img/header_01.jpg" alt="ProEmocion">
-				<img src="<?php echo Yii::app()->request->baseUrl.'/themes//frontEnd'; ?>/img/header_02.jpg" alt="" >			
-			</div>
-		</div>
-	</div>
-</div>
+    <!-- NAVBAR
+    ================================================== -->
+    <div class="navbar-wrapper">
+      <!-- Wrap the .navbar in .container to center it within the absolutely positioned parent. -->
+      <div class="container">
+
+      
+
+      </div> <!-- /.container -->
+    </div><!-- /.navbar-wrapper -->
+
+
+
+    <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide">
+      <div class="carousel-inner">
+        <div class="item active">
+          <div class="aviso"><center><img src="<?php echo Yii::app()->request->baseUrl.'/themes/frontEnd'; ?>/img/header_01.jpg" alt=""></center></div>
+          <div class="container">            
+          </div>
+        </div>
+      </div>     
+    </div><!-- /.carousel -->
+    <div class="clearfix">&nbsp;</div>
+
+
+
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container marketing">
+
+
+      <!-- START THE FEATURETTES -->
+      <div class="featurette">
+        <h2 class="featurette-heading"><span class="muted">Pronto encontrarás aquí las mejores </span>promociones de Zaragoza <span class="muted">en:</span> </h2>
+    </div>
+    <div class="clearfix"><p>&nbsp;</p></div>
+      <div class="featurette well well-small">
+        <h2 class="featurette-heading"><h2>
+       <strong>Belleza</strong><span class="muted">,</span> <strong>Salud y Bienestar</strong><span class="muted">,</span> <strong>Ocio</strong><span class="muted">,</span> <strong>Bares y Restaurantes</strong><span class="muted">,</span> <strong>Hoteles y Alojamientos</strong><span class="muted">,</span> <strong>Compras</strong> <span class="muted">y</span> <strong>Alimentación</strong></h2>
+      </div>
+
+    <div class="clearfix"><p>&nbsp;</p></div>
+
+      <div class="featurette">
+        <h2 class="featurette-heading">Promociones actuales <span class="muted">de los mejores negocios de tu ciudad</span></h2>
+      </div>
+     
+  	      <div class="clearfix"><p>&nbsp;</p></div>
+
+      <div class="featurette">
+        <h2 class="featurette-heading">¡Solo promociones de calidad!</h2>
+      </div>
+
+      <!-- /END THE FEATURETTES -->
+
+    </div><!-- /.container -->
+
+
 
 <div class="clearfix"><p>&nbsp;</p></div>
-<div class="row-fluid">
-	<div class="span12">
-		<div class="aviso">
-			<center><h1>Pronto encontrarás aquí las mejores promociones de Zaragoza en:</h1><br>
-				<h2><strong>Belleza</strong>, <strong>Salud y Bienestar</strong>, <strong>Ocio</strong>, <strong>Bares y Restaurantes</strong>, <strong>Hoteles y Alojamientos</strong>, <strong>Compras</strong> y <strong>Alimentación</strong></h2>
-			<div class="clearfix">&nbsp;</div>
-			<h2>
-				Promociones actuales de los mejores negocios de tu ciudad</h2>
-				<div class="clearfix">&nbsp;</div>
-				<div class="clearfix"></div>
-				<img class="thumbnailimgdest" data-hover="<?php echo Yii::app()->getBaseUrl().Yii::app()->params['no_image_big']; ?>" src="<?php echo Yii::app()->getBaseUrl().Yii::app()->params['no_image_big']; ?>" alt="proemocion">
-				<h2>
-					¡Solo promociones de calidad!</h2>
-					<div class="clearfix">&nbsp;</div>
-			</center>
-		</div>
-	</div>
-</div>
